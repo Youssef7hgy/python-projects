1ff599f35d2e90ddd78d8a59da6242b5f1baad57
Youssef7hgy <youssef.mando123@icloud.com>
2026-07-15 01:16:22 +0300

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,9 @@
+# Calculator Project - By Youssef Khaled
+print("=== Simple Calculator ===")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Sum:", num1 + num2)
+print("Subtract:", num1 - num2)
+print("Multiply:", num1 * num2)
+print("Divide:", num1 / num2)
